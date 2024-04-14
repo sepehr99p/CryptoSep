b485e8e8d3236d3754d4a9899476fb35414c8929
Sep <sepehr.pourekrami@gmail.com>
2024-04-14 13:50:45 +0330

    currency component impl

--- app/src/main/java/com/example/cryptosep/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/cryptosep/ui/screen/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.cryptosep.ui.screen.home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.cryptosep.ui.screen.home.component.currency.CurrencyListComponent
 
 @Composable
 fun HomeScreen() {
@@ -11,6 +12,8 @@ fun HomeScreen() {
     val currencyState = viewModel.currencyList.collectAsState()
     val marketState = viewModel.marketList.collectAsState()
 
-
+    currencyState.value.data?.let {
+        CurrencyListComponent(it)
+    }
 
 }
---
+++ app/src/main/java/com/example/cryptosep/ui/screen/home/component/currency/CurrencyListComponent.kt
@@ -0,0 +1,62 @@
+package com.example.cryptosep.ui.screen.home.component.currency
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cryptosep.domain.entity.CurrencyEntity
+
+@Composable
+fun CurrencyListComponent(
+    modifier: Modifier = Modifier,
+    currencyEntities: List<CurrencyEntity> = listOf()
+) {
+    LazyColumn(modifier = modifier) {
+        items(currencyEntities) {
+            CurrencyItemComponent(currencyEntity = it)
+        }
+    }
+}
+
+
+@Composable
+fun CurrencyItemComponent(modifier: Modifier = Modifier, currencyEntity: CurrencyEntity) {
+    Box(modifier = modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterStart),
+            text = currencyEntity.fullName,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Text(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            text = currencyEntity.precision.toString(),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+
+}
+
+val mockCurrencyEntity = CurrencyEntity(
+    currency = "Currency",
+    name = "name",
+    fullName = "full name",
+    precision = 34
+)
+
+@Preview
+@Composable
+private fun CurrencyItemComponentPreview() {
+    CurrencyItemComponent(currencyEntity = mockCurrencyEntity)
+}
+
+@Preview
+@Composable
+fun CurrencyComponentPreview() {
+    CurrencyListComponent()
+}
