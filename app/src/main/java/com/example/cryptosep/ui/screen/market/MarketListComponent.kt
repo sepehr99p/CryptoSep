13a12ebb07316497c8cdc3ab5c3b13bf7b824a72
Sep <sepehr.pourekrami@gmail.com>
2024-04-15 15:21:53 +0330

    marketList component

--- app/src/main/java/com/example/cryptosep/ui/screen/market/MarketListComponent.kt
+++ app/src/main/java/com/example/cryptosep/ui/screen/market/MarketListComponent.kt
@@ -3,6 +3,9 @@ package com.example.cryptosep.ui.screen.market
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -12,26 +15,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cryptosep.ui.theme.dimen.corner_8
+import com.example.cryptosep.ui.theme.dimen.padding_16
 import com.example.cryptosep.ui.theme.dimen.padding_8
 import com.example.cryptosep.ui.utils.extentions.shadowBackground
 
 @Composable
 fun MarketListComponent(modifier: Modifier = Modifier, marketList: List<String>) {
-    LazyColumn(modifier = modifier) {
+//    LazyColumn(modifier = modifier) {
+//        items(marketList) {
+//            MarketListItemComponent(title = it)
+//        }
+//    }
+    LazyVerticalGrid(columns = GridCells.Fixed(3), content = {
         items(marketList) {
             MarketListItemComponent(title = it)
         }
-    }
+    })
 }
 
 @Composable
 fun MarketListItemComponent(modifier: Modifier = Modifier, title: String) {
     Text(
         modifier = modifier
-            .fillMaxWidth()
             .padding(padding_8)
             .clip(RoundedCornerShape(corner_8))
-            .shadowBackground(),
+            .shadowBackground()
+            .padding(horizontal = padding_16, vertical = padding_8)
+            ,
         text = title,
         color = MaterialTheme.colorScheme.onPrimary
     )
