71e7db564b6e1454bd84d253b54523ded0d4174a
Sep <sepehr.pourekrami@gmail.com>
2024-04-15 17:05:26 +0330

    loading component updated

--- app/src/main/java/com/example/cryptosep/ui/utils/components/LoadingComponent.kt
+++ app/src/main/java/com/example/cryptosep/ui/utils/components/LoadingComponent.kt
@@ -1,21 +1,28 @@
 package com.example.cryptosep.ui.utils.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cryptosep.ui.theme.dimen.padding_40
 
 @Composable
 fun LoadingComponent() {
-    Column {
-        Spacer(modifier = Modifier.height(24.dp))
-        CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
-        Spacer(modifier = Modifier.height(24.dp))
+    Box(modifier = Modifier.fillMaxWidth()) {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.BottomCenter).padding(vertical = padding_40),
+            color = MaterialTheme.colorScheme.onPrimary,
+            trackColor = MaterialTheme.colorScheme.primary
+        )
     }
 
 }
