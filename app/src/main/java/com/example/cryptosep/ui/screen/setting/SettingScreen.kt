d1c7462eed415edec0e2b87cdd45cb8718c39a9b
Sep <sepehr.pourekrami@gmail.com>
2024-05-09 20:09:29 +0330

    settings screen title

--- app/src/main/java/com/example/cryptosep/ui/screen/setting/SettingScreen.kt
+++ app/src/main/java/com/example/cryptosep/ui/screen/setting/SettingScreen.kt
@@ -2,12 +2,16 @@ package com.example.cryptosep.ui.screen.setting
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.cryptosep.ui.theme.Bold_20
+import com.example.cryptosep.ui.theme.Bold_28
 import com.example.cryptosep.ui.theme.dimen.padding_16
 
 @Composable
@@ -16,6 +20,7 @@ fun SettingsScreen() {
     val selectedExchange = remember { mutableStateOf("Kucoin")}
 
     Column(modifier = Modifier.padding(padding_16)) {
+        Text(text = "Settings", style = Bold_20, color = MaterialTheme.colorScheme.onPrimary)
         ExchangeSelection(selectedExchange = selectedExchange, list = viewModel.exchanges)
         SecretKeyInsertion()
     }
