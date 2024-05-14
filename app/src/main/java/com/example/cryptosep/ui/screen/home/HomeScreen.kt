40fc8e56dded94563c8ef259e049f1ee75d2361d
Sep <sepehr.pourekrami@gmail.com>
2024-05-14 17:22:39 +0330

    HomeScreen updated

--- app/src/main/java/com/example/cryptosep/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/cryptosep/ui/screen/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
@@ -19,6 +21,10 @@ import androidx.navigation.NavHostController
 import com.example.cryptosep.R
 import com.example.cryptosep.ui.NavigationItem
 import com.example.cryptosep.ui.theme.dimen.corner_8
+import com.example.cryptosep.ui.theme.dimen.padding_16
+import com.example.cryptosep.ui.theme.dimen.padding_24
+import com.example.cryptosep.ui.theme.dimen.padding_32
+import com.example.cryptosep.ui.theme.dimen.padding_4
 import com.example.cryptosep.ui.theme.dimen.padding_8
 import com.example.cryptosep.ui.utils.extentions.shadowBackground
 
@@ -26,14 +32,22 @@ import com.example.cryptosep.ui.utils.extentions.shadowBackground
 fun HomeScreen(navController: NavHostController) {
 
     Column(modifier = Modifier.fillMaxWidth()) {
-        HomeItems(title = stringResource(id = R.string.ticker)) {
+        TickerItem(title = stringResource(id = R.string.ticker)) {
             navController.navigate(NavigationItem.Tickers.route)
         }
-        HomeItems(title = stringResource(id = R.string.currency)) {
-            navController.navigate(NavigationItem.Currency.route)
-        }
-        HomeItems(title = stringResource(id = R.string.markets)) {
-            navController.navigate(NavigationItem.Market.route)
+        Row {
+            HomeItems(
+                modifier = Modifier.weight(1f),
+                title = stringResource(id = R.string.currency)
+            ) {
+                navController.navigate(NavigationItem.Currency.route)
+            }
+            HomeItems(
+                modifier = Modifier.weight(1f),
+                title = stringResource(id = R.string.markets)
+            ) {
+                navController.navigate(NavigationItem.Market.route)
+            }
         }
         HomeItems(title = stringResource(id = R.string.settings)) {
             navController.navigate(NavigationItem.Settings.route)
@@ -42,28 +56,60 @@ fun HomeScreen(navController: NavHostController) {
 
 }
 
-
 @Composable
-private fun HomeItems(modifier: Modifier = Modifier, title: String, callback: () -> Unit) {
+private fun TickerItem(modifier: Modifier = Modifier, title: String, callback: () -> Unit) {
     val interactionSource = remember { MutableInteractionSource() }
     Box(modifier = modifier
         .fillMaxWidth()
-        .padding(padding_8)
+        .padding(horizontal = padding_8, vertical = padding_4)
         .clip(RoundedCornerShape(corner_8))
         .shadowBackground()
-        .padding(padding_8)
+        .padding(horizontal = padding_8, vertical = padding_32)
         .clickable(
             interactionSource = interactionSource,
             indication = null
         ) {
             callback.invoke()
         }) {
-        Text(text = title, color = MaterialTheme.colorScheme.onPrimary)
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = title,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@Composable
+private fun HomeItems(modifier: Modifier = Modifier, title: String, callback: () -> Unit) {
+    val interactionSource = remember { MutableInteractionSource() }
+    Box(modifier = modifier
+        .fillMaxWidth()
+        .padding(horizontal = padding_8, vertical = padding_4)
+        .clip(RoundedCornerShape(corner_8))
+        .shadowBackground()
+        .padding(horizontal = padding_8, vertical = padding_24)
+        .clickable(
+            interactionSource = interactionSource,
+            indication = null
+        ) {
+            callback.invoke()
+        }) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = title,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
     }
 }
 
 @Preview
 @Composable
-fun HomeItemsPreview() {
+private fun HomeItemsPreview() {
     HomeItems(title = "title") {}
+}
+
+@Preview
+@Composable
+private fun TickerItemPreview(modifier: Modifier = Modifier) {
+    TickerItem(title = "title") {}
 }
