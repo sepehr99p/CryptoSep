21e8db802a8d93e99d9f4fa92f3be48011d0fba9
Sep <sepehr.pourekrami@gmail.com>
2024-05-15 16:55:00 +0330

    settingScreen minor update

--- app/src/main/java/com/example/cryptosep/ui/screen/setting/SettingScreen.kt
+++ app/src/main/java/com/example/cryptosep/ui/screen/setting/SettingScreen.kt
@@ -8,18 +8,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.cryptosep.R
 import com.example.cryptosep.ui.theme.Bold_20
 import com.example.cryptosep.ui.theme.dimen.padding_16
 
 @Composable
 fun SettingsScreen() {
     val viewModel = hiltViewModel<SettingsViewModel>()
-    val selectedExchange = remember { mutableStateOf("Kucoin")}
+    val selectedExchange = remember { mutableStateOf("Kucoin") }
 
     Column(modifier = Modifier.padding(padding_16)) {
-        Text(text = "Settings", style = Bold_20, color = MaterialTheme.colorScheme.onPrimary)
+        Text(
+            text = stringResource(id = R.string.settings),
+            style = Bold_20,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
         ExchangeSelection(selectedExchange = selectedExchange, list = viewModel.exchanges)
         SecretKeyInsertion()
     }
